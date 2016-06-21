4aee62145450105c5ad20d7e943db1222b0456ec
Bruno Neves <brunu.neves@gmail.com>
2016-06-21 20:08:50 -0300

    edit clients

--- resources/views/admin/clients/edit.blade.php
+++ resources/views/admin/clients/edit.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
     <div class="container">
-        <h3> Editar Categoria: {{ $category->name }} </h3>
+        <h3> Editar Cliente: {{ $client->user->name }} </h3>
 
         @include('errors._check')
 
-        {!! Form::model($category, ['route' => ['admin.categories.update', $category->id]]) !!}
+        {!! Form::model($client, ['route' => ['admin.clients.update', $client->id]]) !!}
 
-        @include('admin.categories._form')
+        @include('admin.clients._form')
 
         {!! Form::close() !!}
 
