2d77cba767737d5838e2b80503cc85e771fda78d
Bruno Neves <brunu.neves@gmail.com>
2016-06-21 19:52:29 -0300

    create controller client and request

---
+++ app/Http/Controllers/ClientsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace CodeDelivery\Http\Controllers;
+
+use CodeDelivery\Http\Requests;
+use CodeDelivery\Http\Requests\AdminClientRequest;
+use CodeDelivery\Repositories\ClientRepository;
+
+
+class ClientsController extends Controller
+{
+
+
+    /**
+     * @var ClientRepository
+     */
+    private $repository;
+
+    public function __construct(ClientRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function index()
+    {
+        $clients = $this->repository->paginate();
+        return view('admin.clients.index', compact('$clients'));
+    }
+
+    /**
+     * @return mixed
+     */
+    public function create(){
+        return view('admin.clients.create');
+    }
+
+    /**
+     * @param AdminClientRequest $request
+     * @return mixed
+     */
+    public function store(AdminClientRequest $request)
+    {
+        $data = $request->all();
+        $this->repository->create($data);
+        return redirect()->route('admin.clients.index');
+    }
+
+    /**
+     * @param $id
+     */
+    public function edit($id)
+    {
+        $client = $this->repository->find($id);
+        return view('admin.clients.edit', compact('$client'));
+    }
+
+    public function update(AdminClientRequest $request, $id)
+    {
+        $data = $request->all();
+        $this->repository->update($data, $id);
+        return redirect()->route('admin.clients.index');
+    }
+}
