15bef7eeb7d7c3d4803bb2b8d89f3a639cf5c926
Bruno Neves <brunu.neves@gmail.com>
2016-06-21 20:14:20 -0300

    ajustes form client

--- resources/views/admin/clients/_form.blade.php
+++ resources/views/admin/clients/_form.blade.php
@@ -1,6 +1,36 @@
 <div class="form-group">
     {!! Form::label('name','Nome:') !!}
-    {!! Form::text('name', null, ['class'=>'form-control']) !!}
+    {!! Form::text('user[name]', null, ['class'=>'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('email','Email:') !!}
+    {!! Form::email('user[email]', null, ['class'=>'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('telefone','Telefone:') !!}
+    {!! Form::text('phone', null, ['class'=>'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('endereco','Endereço:') !!}
+    {!! Form::textarea('address', null, ['class'=>'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('cidade','Cidade:') !!}
+    {!! Form::email('city', null, ['class'=>'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('estado','Estado:') !!}
+    {!! Form::email('state', null, ['class'=>'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('cep','CEP:') !!}
+    {!! Form::email('zipcode', null, ['class'=>'form-control']) !!}
 </div>
 
 <div class="form-group">
