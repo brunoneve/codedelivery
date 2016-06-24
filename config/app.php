94e20b79afebe4171f8e7278262da16fd3ee1a7b
Bruno Neves <brunu.neves@gmail.com>
2016-06-23 23:31:33 -0300

    Configurando OAuth2

--- config/app.php
+++ config/app.php
@@ -142,6 +142,10 @@ return [
         Prettus\Repository\Providers\RepositoryServiceProvider::class,
         Bestmomo\Scafold\ScafoldServiceProvider::class,
 
+        LucaDegasperi\OAuth2Server\Storage\FluentStorageServiceProvider::class,
+        LucaDegasperi\OAuth2Server\OAuth2ServerServiceProvider::class,
+
+
         /*
          * Application Service Providers...
          */
@@ -200,6 +204,7 @@ return [
         'View'      => Illuminate\Support\Facades\View::class,
         'Html'      => Illuminate\Html\HtmlFacade::class,
         'Form'      => Illuminate\Html\FormFacade::class,
+        'Authorizer' => LucaDegasperi\OAuth2Server\Facades\Authorizer::class,
 
     ],
 
