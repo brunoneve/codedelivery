f3ebd76f48d0e58f1da6d3dca2d9e18261a93290
Bruno Neves <brunu.neves@gmail.com>
2016-06-29 16:03:30 -0300

    Criando middleware Oauth2

---
+++ app/Http/Middleware/OAuthCheckRole.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace CodeDelivery\Http\Middleware;
+
+use Closure;
+use CodeDelivery\Repositories\UserRepository;
+use LucaDegasperi\OAuth2Server\Facades\Authorizer;
+
+class OAuthCheckRole
+{
+
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $role)
+    {
+        $id = Authorizer::getResourceOwnerId();
+        $user = $this->userRepository->find($id);
+
+        if($user->role != $role){
+            abort(403,'Access Forbidden');
+        }
+        return $next($request);
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -66,12 +66,25 @@ Route::post('oauth/access_token', function() {
 });
 
 Route::group(['prefix' => 'api', 'middleware' => 'oauth', 'as' => 'api.'], function () {
-    Route::get('teste', function(){
-        return [
-            'name' => 1,
-            'client' => 'Bruno Neves',
-            'total' => 10
-        ];
+
+    Route::group(['prefix' => 'client', 'middleware' => 'oauth.checkrole:client', 'as' => 'client.'], function () {
+        Route::get('pedidos', function(){
+            return [
+                'name' => 1,
+                'client' => 'Bruno Coelho',
+                'total' => 10
+            ];
+        });
+    });
+
+    Route::group(['prefix' => 'deliveryman', 'middleware' => 'oauth.checkrole:deliveryman', 'as' => 'deliveryman.'], function () {
+        Route::get('pedidos', function(){
+            return [
+                'name' => 1,
+                'client' => 'Jennifer Coelho',
+                'total' => 10
+            ];
+        });
     });
 
 });
