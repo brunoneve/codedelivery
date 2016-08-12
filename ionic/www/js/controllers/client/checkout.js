6df535e8d83e45f6ca4633b5dafe2e3f64712808
Bruno Neves <brunu.neves@gmail.com>
2016-08-12 00:03:24 -0300

    Remove item cart

--- ionic/www/js/controllers/client/checkout.js
+++ ionic/www/js/controllers/client/checkout.js
@@ -5,4 +5,10 @@ angular.module('starter.controllers')
             var cart = $cart.get();
             $scope.items = cart.items;
             $scope.total = cart.total;
+
+            $scope.removeItem = function (i){
+                $cart.removeItem(i);
+                $scope.items.splice(i, 1);
+                $scope.total = $cart.get().total;
+            }
         }]);
